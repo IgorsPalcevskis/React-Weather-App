0cf75db1b197e1fe2be28a83093a22c7a837024d
Igors Palcevskis <igors.palcevskis@gmail.com>
2019-02-26 20:17:14 +0000

    acquired API key from OpenWeatherMap \ created getWeather method in App.js with API call \ converted received data into JSON using method json()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+
 import Title from './components/Title';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = 'c87ae1ce207b86d32f1ac31b319e04ad';
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
