8f8a6c4b095c5c57ca7f092416f1436d652c1d6e
Igors Palcevskis <igors.palcevskis@gmail.com>
2019-02-26 20:33:15 +0000

    added form with two input elements and button to Form component \ added get weather property to Form component \ passed getWeather property to Form when form element is submitted \ added preventDefault() to getWeather method to avoid page refresh when the form is submitted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Weather from './components/Weather';
 const API_KEY = 'c87ae1ce207b86d32f1ac31b319e04ad';
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
@@ -16,7 +17,7 @@ class App extends React.Component {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
