bdbd733fc8e663525574592e2e516d7769ed4ae5
Igors Palcevskis <igors.palcevskis@gmail.com>
2019-02-26 20:59:52 +0000

    added variables to getWeather method to access Forms input elements name values and modified API URL \ added an initial state to App component \ added setState to change state depending on received data from API

--- src/App.js
+++ src/App.js
@@ -7,11 +7,29 @@ import Weather from './components/Weather';
 const API_KEY = 'c87ae1ce207b86d32f1ac31b319e04ad';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humiduty: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humiduty: data.main.humiduty,
+      description: data.weather[0].description,
+      error: 'error'
+    })
+    console.log(this.state.temperature);
   }
   render() {
     return (
